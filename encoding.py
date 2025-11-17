7945ddfa8970b1fe8edd1c1dacd55db019edcf35
Ming Xu <xumingxs@qq.com>
2025-11-17 21:30:52 +0800

    添加一些pi,pi_inv以及多项式实验

--- encoding.py
+++ encoding.py
@@ -10,6 +10,19 @@ class SimpleEncoding:
         self.roots = [np.exp(2j * np.pi * k / M) for k in range(M) if gcd(k, M) == 1] # 通过分园多项式结论得知：该式子得到X^N+1对应的所有单位根(M=2N)
 
         self.A = np.array([[root ** i for i in range(self.N)] for root in self.roots])  # 构造矩阵A
+        self.AT = self.A.T
+
+        self.orthogonal_basis = []
+        for row in self.AT:
+            poly = np.polynomial.Polynomial(row)
+            self.orthogonal_basis.append(poly)
+
+    def pi(self, z: np.ndarray) -> np.ndarray:
+        return z[:self.N//2]
+    
+    def pi_inv(self, z_half: np.ndarray) -> np.ndarray:
+        v_conj_rev = np.conj(z_half)[::-1]
+        return np.concatenate([z_half, v_conj_rev])
 
     def sigma(self, poly: np.polynomial.Polynomial) -> np.ndarray:
         """典范嵌入映射：多项式系数 -> 向量表示"""
@@ -28,6 +41,17 @@ class SimpleEncoding:
 if __name__ == "__main__":
     M = 8
     encoder = SimpleEncoding(M)
+
+    A = encoder.orthogonal_basis
+
+    # 检验每一行是否正交，需要将行向量转换为多项式
+    for i in range(len(A)):
+        for j in range(i, len(A)):
+            dot_product = np.vdot(A[i].coef, A[j].coef)
+            print(f"行 {i} 和 行 {j} 的点积: {dot_product}")
+            v_i = encoder.sigma(A[i])
+            v_j = encoder.sigma(A[j])
+            print(f"对应多项式的典范嵌入映射点积: {np.vdot(v_i, v_j)}\n")
 
     # 示例多项式：f(x) = 1 + 2x + 3x^2 + 4x^3
     poly = np.polynomial.Polynomial([1, 2, 3, 4])
